f024ccde41ab718637da85a761ca8aa086d6a873
Omer Faruk APLAK <omeraplak@gmail.com>
2019-05-23 12:18:07 +0300

    updated footer logo

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -96,15 +96,14 @@ class Footer extends React.Component {
         </section>
 
         <a
-          href="https://opensource.facebook.com/"
+          href="https://github.com/pankod"
           target="_blank"
           rel="noreferrer noopener"
           className="fbOpenSource">
           <img
             src={`${this.props.config.baseUrl}img/pankod_footer_logo.png`}
-            alt="Facebook Open Source"
+            alt="Pankod Open Source"
             width="170"
-            height="45"
           />
         </a>
         <section className="copyright">{this.props.config.copyright}</section>
